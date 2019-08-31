73567be55977b8dace162a9ed097d83f94e1d99c
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-08-31 10:58:32 -0300

    Add algorithms and gitignore file

---
+++ math/number_theory/prime_numbers/isPrime.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isPrime(int number){
+    for(int i = 2; i <= sqrt(number); i++){
+        if(number % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
