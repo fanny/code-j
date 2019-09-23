0f74e4272fb0efe163b967498640dfbce4e7b624
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-09-22 22:29:16 -0400

    add iteration methods

---
+++ graphs/python/bfs.py
@@ -0,0 +1,14 @@
+
+def bfs(root, graph):
+    V = len(graph.keys())
+    visiteds = [False] * V
+    queue = [root]
+    
+    while(len(queue) != 0):
+        node = queue.pop()
+        visiteds[node] = True
+        for child in graph[node]:
+            if(not visiteds[child]):
+                queue.append(child)
+
+            
