d964bd7cc9349654ad5464157e2537253270b35a
Fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-09-23 13:25:16 -0300

    Update bfs.py

--- graphs/python/bfs.py
+++ graphs/python/bfs.py
@@ -5,7 +5,7 @@ def bfs(root, graph):
     queue = [root]
     
     while(len(queue) != 0):
-        node = queue.pop()
+        node = queue.pop(0)
         visiteds[node] = True
         for child in graph[node]:
             if(not visiteds[child]):
